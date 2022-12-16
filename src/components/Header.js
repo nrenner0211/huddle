58fd2b184134991de96cb6497b0dfe282ac4920c
nrenner <nrenner0211@gmail.com>
2022-12-16 13:35:25 -0500

    logo fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
 import { Flex } from "./styles/Flex.styled";
 import { Container } from "./styles/Container.styled";
 import { Button } from "./styles/Button.styled";
+import logo from "../images/logo.svg";
 
 export default function Header() {
     return (
         <StyledHeader>
             <Container>
                 <Nav>
-                    <Logo src='../images/logo.svg' alt='logo' />
+                    <Logo src={logo} alt='logo' />
                     <Button>Try It Free</Button>
                 </Nav>
 
--- src/content.js
+++ src/content.js
@@ -1,22 +1,26 @@
+import Icon1 from './images/illustration-grow-together.svg';
+import Icon2 from './images/illustration-flowing-conversation.svg';
+import Icon3 from './images/illustration-your-users.svg';
+
 const content = [
     {
         id: 1,
         title: 'Grow Together',
         body: 'Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.',
-        image: 'illustration-grow-together.svg',
+        image: Icon1,
         },
         {
         id: 2,
         title: 'Flowing Conversations',
         body: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-        image: 'illustration-flowing-conversation.svg',
+        image: Icon2,
     },
     {
         id: 3,
         title: 'Your Users',
         body: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
-        image: 'illustration-your-users.svg',
+        image: Icon3,
     },
 ]
 
-export default content
+export default content;
